0c901f8df84ae178b6473e925fea7221ff4561d3
LestarDev <98614691+LestarDev@users.noreply.github.com>
2023-10-19 12:04:42 +0200

    more changes!

--- src/app/shared/components/mineraly/coal.tsx
+++ src/app/shared/components/mineraly/coal.tsx
@@ -1,9 +1,10 @@
 import Mineral from "../../config/mineralInterface";
+import coalPNG from "./../../../../assets/coal.png";
 
 const coal: Mineral = {
     nazwa: 'Węgiel',
     opis: 'Węgiel występuje niemal na całym świecie. Największymi jego wydobywcami są m.in. Chiny, USA, Australia. Ma bardzo szerokie zastosowania - jest używany do produkcji stali, jako źródło energii produkowanej w elektrowniach węglowych, w przemyśle chemicznym do produkcji różnych substancji jak np. syntetyczne paliwo, plastik. Produkcja energii z węgla jest szkodliwa dla środowiska z powodu emisji dwutlenku węgla i innych gazów cieplarnianych.',
-    img: '',
+    img: coalPNG,
     wartosc: 0
 }
 
