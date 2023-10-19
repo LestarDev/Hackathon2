0cc900cdc44e6a303588e00319de76bc38cdb8bd
ProximaX5 <pgesehl@gmail.com>
2023-10-19 10:03:20 +0200

    mineraly

--- src/app/shared/components/mineraly/iron.tsx
+++ src/app/shared/components/mineraly/iron.tsx
@@ -2,7 +2,7 @@ import Mineral from "../../config/mineralInterface";
 
 const iron: Mineral = {
     nazwa: "Żelazo",
-    opis: "",
+    opis: `Występuje powszechnie na całym świecie, największymi producentami są Chiny, Australia, Brazylia. Jest używane do produkcji stali, wykorzystywane w budownictwie np. budowie mostów, konstrukcji metalowych oraz elektronice. Żelazo jest podatne na rdzawienie w obecności wilgoci i tlenu. W odpowiednich warunkach wykazuje właściwości magnetyczne.`,
     img: "",
     wartosc: 0
 }
---
+++ src/app/shared/components/mineraly/steel.tsx
@@ -0,0 +1,10 @@
+import Mineral from "../../config/mineralInterface";
+
+const steel: Mineral = {
+    nazwa: 'Stal',
+    opis: 'Jest to stop żelaza i węgla z niewielką domieszką innych metali takich jak mangan, chrom czy nikiel. Jest to jeden z najważniejszych surowców przemysłowych na świecie, wykrozystywany m.in. w budownictwie, motoryzacji, produkcji maszyn, przemyśle chemicznym i wiele więcej. W odpowiednich warunkach ma właściwości magnetyczne Jest podatna na rdzawienie w obecności wilgoci i tlenu.',
+    img: '',
+    wartosc: 0
+}
+
+export default steel
